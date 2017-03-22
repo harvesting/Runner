2ceb385e03e8229b73a332e99586b4d078a7a358
liaderlich <liaderlich@192.168.1.5>
2017-03-21 23:06:27 -0700

    Added camera, player, and new screen size

--- Runner-core/src/com/mygdx/game/Runner.java
+++ Runner-core/src/com/mygdx/game/Runner.java
@@ -1,33 +1,88 @@
 package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 
 public class Runner extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+	OrthographicCamera cam;
+	private float h, w, rotationSpeed;
+	private Sprite mapSprite;
+	static final int WORLD_WIDTH = 100;
+	static final int WORLD_HEIGHT = 100;
 	
 	@Override
 	public void create () {
+		mapSprite = new Sprite(new Texture("badlogic.jpg"));
+		mapSprite.setPosition(0, 0);
+		mapSprite.setSize(WORLD_WIDTH, WORLD_HEIGHT);
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		w = Gdx.graphics.getWidth();
+		h = Gdx.graphics.getHeight();
+		rotationSpeed = 0.5f;
+		cam = new OrthographicCamera(30, 30 * (h / w));
+		cam.position.set(cam.viewportWidth / 2f, cam.viewportHeight / 2f, 0);
+		cam.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		handleInput();
+		cam.update();
+		batch.setProjectionMatrix(cam.combined);
+
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
 		batch.begin();
-		batch.draw(img, 0, 0);
+		mapSprite.draw(batch);
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
 	}
+	
+	private void handleInput() {
+		if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+			cam.zoom += 0.02;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.Q)) {
+			cam.zoom -= 0.02;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			cam.translate(-3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			cam.translate(3, 0, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			cam.translate(0, -3, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			cam.translate(0, 3, 0);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+			cam.rotate(-rotationSpeed, 0, 0, 1);
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.E)) {
+			cam.rotate(rotationSpeed, 0, 0, 1);
+		}
+
+		cam.zoom = MathUtils.clamp(cam.zoom, 0.1f, 100/cam.viewportWidth);
+
+		float effectiveViewportWidth = cam.viewportWidth * cam.zoom;
+		float effectiveViewportHeight = cam.viewportHeight * cam.zoom;
+
+		cam.position.x = MathUtils.clamp(cam.position.x, effectiveViewportWidth / 2f, 100 - effectiveViewportWidth / 2f);
+		cam.position.y = MathUtils.clamp(cam.position.y, effectiveViewportHeight / 2f, 100 - effectiveViewportHeight / 2f);
+	}
+
 }
